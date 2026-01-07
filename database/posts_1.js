5874e23b263677faa712b829125554a72be5095d
rameztech <ramezalkazzaz@gmail.com>
2026-01-07 23:14:35 +0300

    Update posts - 2026-01-07 23:14

--- database/posts_1.js
+++ database/posts_1.js
@@ -5,9 +5,4 @@ window.postsData = window.postsData || {};
 
 // ندمج البيانات الجديدة في المتغير العام
 Object.assign(window.postsData, {
-    5: {
-        content: `
-<h3>دليل فلاش Xiaomi - Mi Flash Tool</h3><p>شرح شامل لعملية الفلاش الكامل لهواتف شاومي باستخدام الأداة الرسمية.</p>
-`
-    },
 } );
