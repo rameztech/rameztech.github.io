dd908dd8b77397a76a4fc1b05acd13c2c0f48a9f
rameztech <ramezalkazzaz@gmail.com>
2026-01-07 23:44:46 +0300

    Delete database directory

--- database/posts_1.js
+++
@@ -1,8 +0,0 @@
-// database/posts_1.js
-
-// نجعل المتغير عاماً بشكل صريح
-window.postsData = window.postsData || {};
-
-// ندمج البيانات الجديدة في المتغير العام
-Object.assign(window.postsData, {
-} );
