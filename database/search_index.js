c9e93a8b68317ead677f1cfe39deada31d7fdd17
rameztech <ramezalkazzaz@gmail.com>
2026-01-07 23:14:36 +0300

    Update search index - 2026-01-07 23:14

--- database/search_index.js
+++ database/search_index.js
@@ -1,10 +1,2 @@
 window.searchIndex = [
-    { 
-        id: 5, 
-        title: "فلاش هواتف Xiaomi باستخدام Mi Flash Tool", 
-        category: "software", 
-        excerpt: "تعلم كيفية عمل فلاش كامل لهواتف شاومي وحل جميع مشاكل السوفتوير",
-        cover: "https://images.unsplash.com/photo-1574944985070-8f3ebc6b79d2?w=500",
-        file: "posts_1.js"
-    },
 ];
