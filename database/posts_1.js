05736ee482740347004007b4974f412cd0bd7206
rameztech <ramezalkazzaz@gmail.com>
2026-01-07 23:14:10 +0300

    Update posts - 2026-01-07 23:14

--- database/posts_1.js
+++ database/posts_1.js
@@ -5,11 +5,6 @@ window.postsData = window.postsData || {};
 
 // ندمج البيانات الجديدة في المتغير العام
 Object.assign(window.postsData, {
-    2: {
-        content: `
-<h3>شرح Chimera Tool لتخطي FRP</h3><p>أداة Chimera من أقوى الأدوات لتخطي FRP، وفي هذا الشرح سنتعلم كيفية استخدامها بشكل صحيح.</p><img src="https://images.unsplash.com/photo-1616348436168-de43ad0db179?w=800" alt="Chimera Tool"><h4>مميزات Chimera Tool:</h4><ul><li>دعم واسع للأجهزة</li><li>واجهة سهلة الاستخدام</li><li>تحديثات دورية</li></ul>
-`
-    },
     3: {
         content: `
 <h3>إصلاح IMEI لهواتف MediaTek</h3><p>شرح تفصيلي لحل مشكلة IMEI NULL في هواتف MTK باستخدام Miracle Box.</p>
