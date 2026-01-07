4d7cdfaf28905f6d443a8dadcdf6ee653ec24152
rameztech <ramezalkazzaz@gmail.com>
2026-01-08 00:49:59 +0300

    Add files via upload

---
+++ database/categories.js
@@ -0,0 +1,8 @@
+window.siteCategories = {
+    'all': 'الكل',
+    'imei': 'إصلاح IMEI',
+    'frp': 'تخطي FRP',
+    'tools': 'أدوات',
+    'software': 'شروحات سوفتوير',
+    'hardware': 'شروحات هاردوير'
+};
