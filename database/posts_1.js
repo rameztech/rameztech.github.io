2bec24d940ef3656eb6744a283626eb70757ec34
rameztech <ramezalkazzaz@gmail.com>
2026-01-08 00:28:07 +0300

    Add files via upload

---
+++ database/posts_1.js
@@ -0,0 +1,8 @@
+// database/posts_1.js
+
+// نجعل المتغير عاماً بشكل صريح
+window.postsData = window.postsData || {};
+
+// ندمج البيانات الجديدة في المتغير العام
+Object.assign(window.postsData, {
+} );
