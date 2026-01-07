1541c6b26597cda073583caaa416c75eb0faa00f
rameztech <ramezalkazzaz@gmail.com>
2026-01-08 01:02:20 +0300

    Update categories - 2026-01-08

---
+++ database/categories.js
@@ -0,0 +1,9 @@
+window.siteCategories = {
+    "all": "الكل",
+    "imei": "إصلاح IMEI",
+    "frp": "تخطي FRP",
+    "tools": "أدوات",
+    "software": "شروحات سوفتوير",
+    "hardware": "شروحات هاردوير",
+    "test": "ءسبسيب"
+};
