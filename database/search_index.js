dab1277357ef8aab71cecc6f5f00447d704824ae
rameztech <ramezalkazzaz@gmail.com>
2026-01-20 11:48:40 +0000

    Update search index - 2026-01-20 11:49

--- database/search_index.js
+++ database/search_index.js
@@ -55,4 +55,14 @@ window.searchIndex = [
         cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_7_20260108_224728.jpg",
         file: "posts_1.js"
     },
+    { 
+        id: 8, 
+        title: "Redmi 14c lake/pond", 
+        category: "imei", 
+        excerpt: "Hayper os1 اصلاح ايمي  
+    بدون فورمات
+     DFT PRO",
+        cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_8_20260120_114944.jpg",
+        file: "posts_1.js"
+    },
 ];
