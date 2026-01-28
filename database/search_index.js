2438e8ebd690f41a0cfd91443615d5b0c00c0ac5
rameztech <ramezalkazzaz@gmail.com>
2026-01-28 14:32:51 +0000

    Update search index - 2026-01-28 14:34

--- database/search_index.js
+++ database/search_index.js
@@ -63,4 +63,12 @@ window.searchIndex = [
         cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_8_20260121_114800.jpg",
         file: "posts_1.js"
     },
+    { 
+        id: 9, 
+        title: "redmi 13 moon اصلاح ايمي", 
+        category: "frp", 
+        excerpt: "مقفل بوت لودر   سيرفر  + dft pro",
+        cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_9_20260128_143402.jpg",
+        file: "posts_1.js"
+    },
 ];
