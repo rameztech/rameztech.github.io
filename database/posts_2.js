206ed2824e421ecddddcf7281e9cde51c1570ea2
rameztech <ramezalkazzaz@gmail.com>
2026-05-22 20:46:05 +0100

    Update database/posts_2.js - 2026-05-22 22:46

--- database/posts_2.js
+++ database/posts_2.js
@@ -41,6 +41,27 @@ Object.assign(window.postsData, {
     إذا كان جهازك غير مدعوم في DFT Pro أو الأدوات الأخرى، يمكنك طلب خدمة السيرفر المتقدمة.
     <a href="#contact">تواصل معنا للحصول على الخدمة</a>
 </p>
+`
+    },
+    23: {
+        content: `
+<h3>إصلاح الـ IMEI لجهاز Redmi 13 (Moon) os1</h3>
+
+<ul>
+    <li>رقعة الأمان: تحديث 2024-11-01</li>
+    <li>   تحديد نوع الحماية: في خانة Security Type، قم باختيار sec_type3، هذا الخيار ضروري جداً لتتوافق الأداة مع ثغرة الحماية الخاصة بهذا الإصدار من HyperOS   </li>
+</ul>
+
+<ul>
+    <li> Patch BL Lock </li>
+    <li>اضغط على زر IMEI Repair</li>
+</ul>
+<img src="https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/posts/post_23_img1_20260522_224556.jpg" alt="صورة" style="max-width: 100%; border-radius: 10px; margin: 20px 0;">
+<p style="background: #fef3c7; padding: 15px; border-radius: 10px; margin-top: 20px;">
+    <strong>⚠️ للأجهزة غير المدعومة:</strong><br>
+    إذا كان جهازك غير مدعوم في DFT Pro أو الأدوات الأخرى، يمكنك طلب خدمة السيرفر المتقدمة.
+    <a href="#contact">تواصل معنا للحصول على الخدمة</a>
+</p>
 
 `
     },
