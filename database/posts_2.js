e4e13b8a87ac0efd213ff8deaa34c90fd274402a
rameztech <ramezalkazzaz@gmail.com>
2026-05-22 20:29:56 +0100

    Update database/posts_2.js - 2026-05-22 22:29

--- database/posts_2.js
+++ database/posts_2.js
@@ -21,6 +21,26 @@ Object.assign(window.postsData, {
     إذا كان جهازك غير مدعوم في DFT Pro أو الأدوات الأخرى، يمكنك طلب خدمة السيرفر المتقدمة.
     <a href="#contact">تواصل معنا للحصول على الخدمة</a>
 </p>
+`
+    },
+    22: {
+        content: `
+<ul>
+    <li>huawei y6p</li>
+    <li>كما هو موضح بالصور</li>
+</ul>
+
+<ul>
+    <li> med-lx9 reset frp </li>
+    <li> حذف حساب غوغل بالكامل عبر وضع brom باستخدام dft pro </li>
+</ul>
+<img src="https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/posts/post_22_img1_20260522_222946.jpg" alt="صورة" style="max-width: 100%; border-radius: 10px; margin: 20px 0;">
+<img src="https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/posts/post_22_img2_20260522_222949.jpg" alt="صورة" style="max-width: 100%; border-radius: 10px; margin: 20px 0;">
+<p style="background: #fef3c7; padding: 15px; border-radius: 10px; margin-top: 20px;">
+    <strong>⚠️ للأجهزة غير المدعومة:</strong><br>
+    إذا كان جهازك غير مدعوم في DFT Pro أو الأدوات الأخرى، يمكنك طلب خدمة السيرفر المتقدمة.
+    <a href="#contact">تواصل معنا للحصول على الخدمة</a>
+</p>
 
 `
     },
