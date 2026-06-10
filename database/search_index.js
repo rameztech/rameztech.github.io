c7418bf2616eb2e6447f811540b4ae5a79e26663
rameztech <ramezalkazzaz@gmail.com>
2026-06-11 00:23:49 +0300

    Update search index - 2026-06-11 00:23

--- database/search_index.js
+++ database/search_index.js
@@ -183,4 +183,12 @@ window.searchIndex = [
         cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_26_20260610_231818.jpg",
         file: "posts_2.js"
     },
+    { 
+        id: 27, 
+        title: ">تخطي حساب غوغل redmi 12 fire", 
+        category: "frp", 
+        excerpt: "dft pro",
+        cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_27_20260611_002336.jpg",
+        file: "posts_2.js"
+    },
 ];
