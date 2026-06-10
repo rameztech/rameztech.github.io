559c72a686369252b09c8b7d3bdb397e30b5c951
rameztech <ramezalkazzaz@gmail.com>
2026-06-10 19:01:11 +0300

    Remove trailing newline in search_index.js

--- database/search_index.js
+++ database/search_index.js
@@ -167,5 +167,4 @@ window.searchIndex = [
         cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_24_20260522_230835.jpg",
         file: "posts_2.js"
     },
-
 ];
