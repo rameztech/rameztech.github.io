d3abb1923a5443d234c753340d5b9230e37704f4
rameztech <ramezalkazzaz@gmail.com>
2026-07-18 02:03:57 +0300

    Update search index - 2026-07-18 02:03

--- database/search_index.js
+++ database/search_index.js
@@ -207,4 +207,12 @@ window.searchIndex = [
         cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_29_20260718_020105.jpg",
         file: "posts_2.js"
     },
+    { 
+        id: 30, 
+        title: "dvdzvdzv", 
+        category: "imei", 
+        excerpt: "fdfdfdfs",
+        cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_30_20260718_020344.jpg",
+        file: "posts_2.js"
+    },
 ];
