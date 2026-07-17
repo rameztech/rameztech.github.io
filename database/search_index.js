41676a42e99644486f9cb37106674a214ac21194
rameztech <ramezalkazzaz@gmail.com>
2026-07-18 02:41:41 +0300

    Update search index - 2026-07-18 02:41

--- database/search_index.js
+++ database/search_index.js
@@ -199,12 +199,4 @@ window.searchIndex = [
         cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_28_20260612_162323.jpg",
         file: "posts_2.js"
     },
-    { 
-        id: 29, 
-        title: "vddddsf", 
-        category: "hardware", 
-        excerpt: "sdfsfsf",
-        cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_29_20260718_024000.jpg",
-        file: "posts_2.js"
-    },
 ];
