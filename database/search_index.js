9c2dd5b9f70456d918cdca02c929d700fe6defc9
rameztech <ramezalkazzaz@gmail.com>
2026-07-18 01:21:08 +0300

    Update search index - 2026-07-18 01:21

--- database/search_index.js
+++ database/search_index.js
@@ -207,12 +207,4 @@ window.searchIndex = [
         cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_29_20260718_011356.jpg",
         file: "posts_2.js"
     },
-    { 
-        id: 30, 
-        title: "test2", 
-        category: "imei", 
-        excerpt: "dfdfdfdf",
-        cover: "https://raw.githubusercontent.com/rameztech/rameztech.github.io/main/images/covers/cover_post_30_20260718_011637.jpg",
-        file: "posts_2.js"
-    },
 ];
